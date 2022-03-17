b0d06afa9ac53b2723e71186858a40c2e760cf9d
xencodes <xencodes@icloud.com>
2022-03-17 12:03:00 +0530

    fix: update port

--- src/extension.ts
+++ src/extension.ts
@@ -1,94 +1,92 @@
-import * as vscode from "vscode";
-import * as ws from "ws";
-let lastUserId = "";
+import * as vscode from 'vscode';
+import * as ws from 'ws';
+let lastUserId = '';
 
-const websocket = new ws("http://api.xen.codes:9000/");
+const websocket = new ws('https://api.xen.codes:3600');
 const userIdStatus = vscode.window.createStatusBarItem(
-  vscode.StatusBarAlignment.Right,
-  9900
+	vscode.StatusBarAlignment.Right,
+	9900
 );
-userIdStatus.text = "Waiting for ID...";
+userIdStatus.text = 'Waiting for ID...';
 userIdStatus.show();
 
 // Websocket handlers
-websocket.on("open", () => {
-  console.log("[CopyDock] connected to websocket");
+websocket.on('open', () => {
+	console.log('[CopyDock] connected to websocket');
 });
-websocket.on("error", () => {
-  websocket.close();
-  vscode.window.showErrorMessage("Disconnected from CopyDock");
+websocket.on('error', () => {
+	websocket.close();
+	vscode.window.showErrorMessage('Disconnected from CopyDock');
 });
-websocket.on("close", () => {
-  userIdStatus.text = "Disconnected";
-  vscode.window.showErrorMessage("Disconnected from CopyDock");
-});
-
-websocket.on("message", async (wsdata: string) => {
-  try {
-    let { type, payload } = JSON.parse(wsdata);
-
-    switch (type) {
-      case "user-id":
-        userIdStatus.tooltip = `Share "${payload}" with your friend`;
-        userIdStatus.text = `ID: ${payload}`;
-        break;
-
-      case "user-code":
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-          return;
-        }
-        let selection = editor.selection;
-        if (!selection) {
-          return;
-        }
-      
-        editor.edit((builder) => {
-          builder.replace(selection, payload);
-        });
-        break;
-
-      case "error":
-        vscode.window.showErrorMessage(payload);
-        break;
-    }
-  } catch (e) {
-    console.log(`[copydock] error parsing data`);
-  }
+websocket.on('close', () => {
+	userIdStatus.text = 'Disconnected';
+	vscode.window.showErrorMessage('Disconnected from CopyDock');
 });
 
+websocket.on('message', async (wsdata: string) => {
+	try {
+		let { type, payload } = JSON.parse(wsdata);
+
+		switch (type) {
+			case 'user-id':
+				userIdStatus.tooltip = `Share "${payload}" with your friend`;
+				userIdStatus.text = `ID: ${payload}`;
+				break;
+
+			case 'user-code':
+				const editor = vscode.window.activeTextEditor;
+				if (!editor) {
+					return;
+				}
+				let selection = editor.selection;
+				if (!selection) {
+					return;
+				}
+
+				editor.edit((builder) => {
+					builder.replace(selection, payload);
+				});
+				break;
+
+			case 'error':
+				vscode.window.showErrorMessage(payload);
+				break;
+		}
+	} catch (e) {
+		console.log(`[copydock] error parsing data`);
+	}
+});
 
 export function activate(context: vscode.ExtensionContext) {
-  
-  let disposable = vscode.commands.registerTextEditorCommand(
-    "copydock.sendCode",
-    async () => {
-      const workbenchConfig = vscode.workspace.getConfiguration();
-      const settings: any = workbenchConfig.get("copyDock");
+	let disposable = vscode.commands.registerTextEditorCommand(
+		'copydock.sendCode',
+		async () => {
+			const workbenchConfig = vscode.workspace.getConfiguration();
+			const settings: any = workbenchConfig.get('copyDock');
 
-      const editor = vscode.window.activeTextEditor;
-      if (!editor) {
-        return; // No open text editor
-      }
+			const editor = vscode.window.activeTextEditor;
+			if (!editor) {
+				return; // No open text editor
+			}
 
-      const selection = editor.selection;
-      const text = editor.document.getText(selection);
+			const selection = editor.selection;
+			const text = editor.document.getText(selection);
 
-      const otherUserId = await vscode.window.showInputBox({
-        title: "Enter User ID",
-        value: settings.useLastUserId ? lastUserId : "",
-      });
+			const otherUserId = await vscode.window.showInputBox({
+				title: 'Enter User ID',
+				value: settings.useLastUserId ? lastUserId : '',
+			});
 
-      lastUserId = String(otherUserId); // "cache" the last entered user id
-      const data = {
-        otherUserId,
-        text,
-      };
-      websocket.send(JSON.stringify(data));
-    }
-  );
+			lastUserId = String(otherUserId); // "cache" the last entered user id
+			const data = {
+				otherUserId,
+				text,
+			};
+			websocket.send(JSON.stringify(data));
+		}
+	);
 
-  context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
